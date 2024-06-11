5ba56d28c151045603a0ccad4d1c87169781923f
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-06-11 19:54:30 +0600

    Add export excel action

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/import-xml',[\App\Http\Controllers\XMLImportController::class,'import'])->name('import-xml');
+Route::post('/import-xml',[\App\Http\Controllers\XMLImportController::class,'import'])->name('import.xml');
+
+Route::get('/export-excel',[\App\Http\Controllers\ExcelExportController::class,'export'])->name('export.excel');
