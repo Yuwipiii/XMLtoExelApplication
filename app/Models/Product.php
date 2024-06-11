0e977a1620de47e4ece8494fffb820483622f781
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-06-11 15:40:35 +0600

    create Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category',
+        'sub_category',
+        'sub_sub_category',
+        'available',
+        'url',
+        'price',
+        'picture',
+        'name',
+        'currencyId',
+        'old_price'
+    ];
+}
